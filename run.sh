8e0d5b90779acd7c4b706de1a5fd578e635d46cc
Mayamee <ponyashcat228@gmail.com>
2023-06-06 00:55:42 +0300

    rename

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-# common vars
-wsl='/etc/wsl.conf'
-resolv='/etc/resolv.conf'
+WSL_PATH='/etc/wsl.conf'
+RESOLV_PATH='/etc/resolv.conf'
 
 # remove standard files
-rm -f $wsl
-rm -f $resolv
+rm -f $WSL_PATH
+rm -f $RESOLV_PATH
 
 # Create custom WSL name resolution
-cp ./dist/wsl.conf $wsl
-cp ./dist/resolv.conf $resolv
+sudo cp ./dist/wsl.conf $WSL_PATH
+sudo cp ./dist/resolv.conf $RESOLV_PATH
 
 # This prevents resolv.conf from being deleted when WSL starts
-chattr +i $resolv
+sudo chattr +i $RESOLV_PATH
 
 echo 'WSL name resolution configured'
 echo 'Restart WSL on Windows: "wsl --shutdown"'
